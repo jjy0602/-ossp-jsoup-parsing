11fa96cd80d2ec4609bf1de17a57a751a4909b87
jjy0602 <enter4836@naver.com>
2018-01-14 22:21:19 +0900

    jsoupparssing 기본 소스 코드
    파싱 기본 소스 코드
    (일베 사이트 댓글 파싱 )

--- jsoup parssing/src/parssing/jsoupparssing.java
+++ jsoup parssing/src/parssing/jsoupparssing.java
@@ -1,10 +1,21 @@
 package parssing;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class jsoupparssing {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		String articleURL = "http://www.ilbe.com/10292553017";
+		Document doc = Jsoup.connect(articleURL).get();
+		
+		Elements ele = doc.select(".replyContent");
+		String str = ele.text();
+		System.out.println(str);
 	}
 
 }
