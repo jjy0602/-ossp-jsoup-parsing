a38784d4ebf9a8c1aa208de8c22e7b581647de4c
jjy0602 <enter4836@naver.com>
2018-01-16 23:37:11 +0900

    url & tag 입력
    url & tag 을 직접 입력 받아서 대글 파싱 및 단어 검색과 유무 확인

--- jsoup parssing/src/parssing/jsoupparssing.java
+++ jsoup parssing/src/parssing/jsoupparssing.java
@@ -1,38 +1,52 @@
 package parssing;
 
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.util.Scanner;
+import java.io.*;
 
 public class jsoupparssing {
-
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		String articleURL = "http://www.ilbe.com/10292553017";
-		Document doc = Jsoup.connect(articleURL).get();
 		
-		Elements ele = doc.select(".replyContent");//replyContent 클래스 태그 안에 내용 파싱
-		String str = ele.text(); //파싱한 내용을 text 형태로 str에 저장.
+		InputStreamReader Inputvalue = new InputStreamReader(System.in);
 		
-String arr[] = str.split(" "); //공백을 기준으로 문자열 나눔.
+		BufferedReader buff = new BufferedReader(Inputvalue);
+		
+		System.out.print("URL을 입력하세요. : ");
+		String url = buff.readLine();
+		
+		System.out.print("원하는 태그를 입력하세요. : ");
+		String tag = buff.readLine();
+		
+		//String articleURL = "http://www.ilbe.com/10292553017";
+		Document doc = Jsoup.connect(url).get();
+		
+		Elements ele = doc.select(tag);//replyContent 클래스 태그 안에 내용 파싱
+		String output ="";
+		for(Element e:ele)
+		{
+			output +=e.text();
+			output +="\n";
+		}
+		
+		System.out.println(output);
+		
+		
+		//String str = ele.text(); //파싱한 내용을 text 형태로 str에 저장.
+		/*String arr[] = output.split(" "); //공백을 기준으로 문자열 나눔.
 		
 		for(int i =0; i<arr.length ; i++)
 		{
 		System.out.println(arr[i]);
-		}
-		
+		}	*/	
 		Scanner a = new Scanner(System.in);
 		System.out.println("검색할 단어를 입력하세요. : ");
 		
 		while(a.hasNextLine()){ //다음줄 입력 여부 결정
-		
 		String msg = a.nextLine(); //키보드 입력
-		
-		
-		if(str.contains(msg)){ //str 즉 파싱한 댓글에 msg(입력값)에 해당하는 값 탐색
+		if(output.contains(msg)){ //str 즉 파싱한 댓글에 msg(입력값)에 해당하는 값 탐색
 			System.out.println("해당 단어가 포함된 댓글이 있습니다.");
 			System.out.println("다음 검색할 단어를 입력하세요. : ");
 			}
